23884aabb92886056ef08ffbdd6e3a94d6870bf4
rquinzio <remi.quinzio@outlook.fr>
2021-07-04 14:55:10 +0200

    modified:   run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ docker network create blog_network --subnet 172.30.0.0/16 --ip-range 172.30.0.0/
 docker network create wiki_network --subnet 172.32.0.0/16 --ip-range 172.32.0.0/24
 docker network create le_network --subnet 172.33.0.0/16 --ip-range 172.33.0.0/24
 
-docker-compose up
+docker-compose up --force-recreate
